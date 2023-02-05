cfa4567dd340743b2ab72874dc064d0d0b78e8fb
ashsii <ashsiiashsii@outlook.com>
2023-02-05 22:52:15 +0930

    Update etl_web_to_gcs.py

--- week2/question4/etl_web_to_gcs.py
+++ week2/question4/etl_web_to_gcs.py
@@ -33,7 +33,7 @@ def clean(df: pd.DataFrame) -> pd.DataFrame:
 @task()
 def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
     """Write DataFrame out locally as parquet file"""
-    path = Path(f"data/{color}/{dataset_file}.parquet")
+    path = Path(f"data/{color}/{dataset_file}.parquet").as_posix()
     df.to_parquet(path, compression="gzip")
     return path
 
@@ -49,9 +49,6 @@ def write_gcs(path: Path) -> None:
 @flow()
 def etl_web_to_gcs(year: int, month: int, color: str) -> None:
     """The main ETL function"""
-    color = color
-    year = year
-    month = month
     dataset_file = f"{color}_tripdata_{year}-{month:02}"
     dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
 
@@ -62,7 +59,7 @@ def etl_web_to_gcs(year: int, month: int, color: str) -> None:
 
 
 if __name__ == "__main__":
-    color = 'green'
+    color = "green"
     year = 2020
-    month = 1
-    etl_web_to_gcs(year,month,color)
+    month = 11
+    etl_web_to_gcs(color,month,color)
