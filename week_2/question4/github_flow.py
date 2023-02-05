185b97dc2b876eafacc26831578015999fec17b6
ashsii <ashsiiashsii@outlook.com>
2023-02-05 23:25:43 +0930

    Update github_flow.py

--- week_2/question4/github_flow.py
+++ week_2/question4/github_flow.py
@@ -5,13 +5,13 @@ import shutil
 from prefect.deployments import Deployment
 
 script_path = os.path.realpath(os.path.dirname(__file__))
-github_path = "week2/question4"
+github_path = "week_2/question4"
 python_flow = "etl_web_to_gcs.py"
 github_block = GitHub.load("zoom-github")
 
 if __name__ == "__main__":
     # Get data
-    github_block.get_directory(from_path=Path('week2/question4'),local_path=Path(script_path))
+    github_block.get_directory(from_path=Path('week_2/question4'),local_path=Path(script_path))
 
     # Move data to directory
     try:
@@ -21,7 +21,7 @@ if __name__ == "__main__":
         Path(f"{script_path}/{github_path}/{python_flow}").rename(f"{script_path}/{python_flow}")
 
     # Delete reduntant folder
-    shutil.rmtree(f"{script_path}/week2")
+    shutil.rmtree(f"{script_path}/week_2")
 
     # Import from py
     from etl_web_to_gcs import etl_web_to_gcs
@@ -31,6 +31,6 @@ if __name__ == "__main__":
         flow=etl_web_to_gcs,
         name="github-etl-to-gcs",
         storage=github_block,
-        entrypoint="week2/question4/etl_web_to_gcs.py:etl_web_to_gcs")
+        entrypoint="week_2/question4/etl_web_to_gcs.py:etl_web_to_gcs")
 
     deployment.apply()
